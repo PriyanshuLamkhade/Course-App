7e646c3fdd68d56de5fbec69eb0e8c519587d673
Priyanshu <priyanshulamkhade@gmail.com>
2025-01-24 13:28:34 +0530

    excluding the password and email from creator reference

--- routes/course.js
+++ routes/course.js
@@ -27,7 +27,7 @@ courseRouter.post("/purchases",userMiddleware, async function (req, res){
 })
 
 courseRouter.get("/preview", async function (req, res) {
-    const courses= await courseModel.find({}).populate('creatorId')
+    const courses= await courseModel.find({}).populate('creatorId',"-password -email")
 
     res.json({
         courses : courses
